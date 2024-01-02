fb5f2544354a7bc3a8ca21464d0f617a1a83db25
soandsoso <51219603+soandsoso@users.noreply.github.com>
2024-01-03 01:58:13 +0800

    Update diy.sh

--- devices/ipq60xx_generic/diy.sh
+++ devices/ipq60xx_generic/diy.sh
@@ -2,25 +2,25 @@
 shopt -s extglob
 
 rm -rf package/boot/uboot-envtools package/kernel package/firmware/ath11k* package/qca target/linux/generic target/linux/ipq60xx package/network/config/netifd feeds/kiddin9/fullconenat toolchain tools/squashfskit4
-svn export --force https://github.com/coolsnowwolf/openwrt-gl-ax1800/trunk/package/boot/uboot-envtools package/boot/uboot-envtools
-svn export --force https://github.com/coolsnowwolf/openwrt-gl-ax1800/trunk/package/firmware/ath11k-firmware package/firmware/ath11k-firmware
-svn export --force https://github.com/coolsnowwolf/openwrt-gl-ax1800/trunk/package/qca package/qca
+svn export --force https://github.com/soandsoso/ipq60xx/trunk/package/boot/uboot-envtools package/boot/uboot-envtools
+svn export --force https://github.com/soandsoso/ipq60xx/trunk/package/firmware/ath11k-firmware package/firmware/ath11k-firmware
+svn export --force https://github.com/soandsoso/ipq60xx/trunk/package/qca package/qca
 
-svn co https://github.com/coolsnowwolf/openwrt-gl-ax1800/trunk/target/linux/generic target/linux/generic
-svn co https://github.com/coolsnowwolf/openwrt-gl-ax1800/trunk/target/linux/ipq60xx target/linux/ipq60xx
-wget -P dl/ https://github.com/coolsnowwolf/openwrt-gl-ax1800/raw/master/dl/linux-4.4.60.tar.xz
-svn co https://github.com/coolsnowwolf/openwrt-gl-ax1800/trunk/package/kernel package/kernel
-svn co https://github.com/coolsnowwolf/openwrt-gl-ax1800/trunk/package/network/services/fullconenat feeds/kiddin9/fullconenat
-svn co https://github.com/coolsnowwolf/openwrt-gl-ax1800/trunk/package/network/config/netifd package/network/config/netifd
-svn co https://github.com/coolsnowwolf/openwrt-gl-ax1800/trunk/toolchain toolchain
-svn co https://github.com/coolsnowwolf/openwrt-gl-ax1800/trunk/tools/squashfskit4 tools/squashfskit4
+svn co https://github.com/soandsoso/ipq60xx/trunk/target/linux/generic target/linux/generic
+svn co https://github.com/soandsoso/ipq60xx/trunk/target/linux/ipq60xx target/linux/ipq60xx
+wget -P dl/ https://github.com/soandsoso/ipq60xx/raw/master/dl/linux-4.4.60.tar.xz
+svn co https://github.com/soandsoso/ipq60xx/trunk/package/kernel package/kernel
+svn co https://github.com/soandsoso/ipq60xx/trunk/package/network/services/fullconenat feeds/kiddin9/fullconenat
+svn co https://github.com/soandsoso/ipq60xx/trunk/package/network/config/netifd package/network/config/netifd
+svn co https://github.com/soandsoso/ipq60xx/trunk/toolchain toolchain
+svn co https://github.com/soandsoso/ipq60xx/trunk/tools/squashfskit4 tools/squashfskit4
 rm -rf target/linux/generic/files
 rm -rf package/network/config/netifd/patches
 
 sed -i "s/PKG_SOURCE_DATE:=.*/PKG_SOURCE_DATE:=2024-12-06/" package/network/config/netifd/Makefile
 
-curl -sfL https://raw.githubusercontent.com/coolsnowwolf/openwrt-gl-ax1800/master/include/netfilter.mk -o include/netfilter.mk
-curl -sfL https://raw.githubusercontent.com/coolsnowwolf/openwrt-gl-ax1800/master/include/quilt.mk -o include/quilt.mk
+curl -sfL https://raw.githubusercontent.com/soandsoso/ipq60xx/master/include/netfilter.mk -o include/netfilter.mk
+curl -sfL https://raw.githubusercontent.com/soandsoso/ipq60xx/master/include/quilt.mk -o include/quilt.mk
 
 rm -rf toolchain/.svn
 rm -rf feeds/kiddin9/{rtl*,fullconenat-nft,shortcut-fe,netifd} package/kernel/mt76 toolchain/musl package/feeds/packages/{xtables-addons,openvswitch} package/libs/elfutils package/utils/util-linux package/feeds/luci/ucode-mod-html package/libs/openssl package/network/utils/iptables package/feeds/packages/v4l2loopback package/feeds/packages/jool package/network/utils/uqmi devices/common/patches/mt7922.patch
